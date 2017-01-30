0a560de727865499991486eb35b1cee3504f47cc
uoitstudent <billy.wyers@uoit.net>
2017-01-30 09:23:43 -0800

    Added CSV demo and maven repository.

--- lab02/src/main/java/csci202u/lab02/SimpleTest.java
+++ lab02/src/main/java/csci202u/lab02/SimpleTest.java
@@ -1,5 +1,10 @@
 package lab02;
 
-public class HelloWorld{
-	System.out.println("Hello World");
+
+public class SimpleTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
 }
